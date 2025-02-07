bec0726379384b2bb98ecab9a22661758d316b27
Codngwthubaid <codngwthubaid@gmail.com>
2025-02-08 00:33:17 +0530

    Init navbar and hero-section

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { HeroSection } from '@/components/sections/hero'
+
+
+export const metadata = {
+  title: "Agency - Landing template",
+  description: "Landing template for Agency",
+}
+const page = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  )
+}
+
+export default page
