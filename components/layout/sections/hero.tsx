f46cf30385a9787fea3c70f6d8d052f4d70cec9f
Codngwthubaid <codngwthubaid@gmail.com>
2025-02-15 16:39:01 +0530

    Resolving bugs

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -16,11 +16,11 @@ export const HeroSection = () => {
   }, []);
 
   if (!mounted) {
-    return null; 
+    return null;
   }
   return (
     <section className="container w-full px-10 mx-auto">
-      <div className="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
+      <div className="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-24">
         <div className="text-center space-y-8">
           <Badge variant="outline" className="text-sm py-2">
             <span className="mr-2 text-primary">
@@ -31,9 +31,9 @@ export const HeroSection = () => {
 
           <div className="max-w-screen-md mx-auto text-center text-2xl md:text-6xl font-bold">
             <h1>
-              Transform your ideas to digital 
+              Transform your ideas to digital
               <span className="text-transparent px-2 bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
-              perfection
+                perfection
               </span>
               with WebZaura
             </h1>
@@ -44,23 +44,12 @@ export const HeroSection = () => {
           </p>
 
           <div className="space-y-4 md:space-y-0 md:space-x-4">
-            <Button className="w-5/6 md:w-1/4 font-bold group/arrow">
-              Get Started
-              <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
-            </Button>
-
-            <Button
-              asChild
-              variant="secondary"
-              className="w-5/6 md:w-1/4 font-bold"
-            >
-              <Link
-                href="https://github.com/Codngwthubaid"
-                target="_blank"
-              >
-                Github respository
-              </Link>
-            </Button>
+            <Link href="#pricing">
+              <Button className="w-5/6 md:w-1/4 font-bold group/arrow">
+                Get Started
+                <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
+              </Button>
+            </Link>
           </div>
         </div>
 
