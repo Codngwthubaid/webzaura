0b304fcf5c9a9d984597465d992a0bb8809d9783
Codngwthubaid <codngwthubaid@gmail.com>
2025-02-16 11:16:45 +0530

    Resolving bugs

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -4,7 +4,6 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import { useTheme } from "next-themes";
-import Image from "next/image";
 import Link from "next/link";
 import { SparklesText } from "@/components/magicui/sparkles-text"
 import TitledImg from "@/components/animations/titledImg"
