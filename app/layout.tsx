d216ab834ea3e8cf132679f14ecf11a0ae144b7e
Codngwthubaid <codngwthubaid@gmail.com>
2025-02-21 20:38:54 +0530

    Resolves error

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         >
           <Navbar />
           {children}
-          {/* <SplashCursor /> */}
+          <SplashCursor />
           <Chatbot />
           <WhatsAppIcon />
           <Toaster />
